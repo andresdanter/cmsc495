76c963050728bb50ac72e61873663bf0a3348b07
monster99d2 <75825718+monster99d2@users.noreply.github.com>
2022-10-03 23:17:21 -0400

    Update main.js

--- src/vue/weathervane-ui/src/main.js
+++ src/vue/weathervane-ui/src/main.js
@@ -4,27 +4,31 @@ import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter, createWebHashHistory } from "vue-router";
 import Search from './components/Search.vue'
 import Forecast from './components/Forecast.vue'
 import Travelcast from './components/Travelcast.vue'
+import { createAuth0, authGuard } from '@auth0/auth0-vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home', 
+    name: 'Search', 
     component: Search,
+    beforeEnter: authGuard
   },
   {
     path: '/forecast/:address/:date/:datePassed',
     name: 'Forecast',
-    component: Forecast
+    component: Forecast,
+    beforeEnter: authGuard
   },
   {
     path: '/travelcast/:addresses/:date/:datePassed',
     name: 'Travelcast',
-    component: Travelcast
-  },
+    component: Travelcast,
+    beforeEnter: authGuard
+  }
 ];
 
 const router = createRouter({
@@ -40,4 +44,14 @@ const vuetify = createVuetify({
 const app = createApp(App)
 app.use(vuetify)
 app.use(router)
+
+app.use(
+  createAuth0({
+    domain: "dev-9hy7zhkb.us.auth0.com",
+    client_id: "FrRBmDxaWxpZAPVJmG4sQ2jg9zia5WYI",
+    redirect_uri: window.location.origin,
+    audience: "https://weathervaneapp.com/api"
+  })
+);
+
 app.mount('#app')
